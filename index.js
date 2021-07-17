e45f2f7718e0841f4607746496a81ef236792a15
quadral09 <87515678+quadral09@users.noreply.github.com>
2021-07-17 08:53:07 +0200

    First Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js')
+const bot = new Discord.Client()
+const TOKEN = 'ODY1NTQ1ODMzNTA4MzcyNDkw.YPFkXw._Yd1AwCnELn2_SJCt3q8KEqxRoA'
+const prefix = 't!'
+
+bot.on('ready', () => {
+    console.log('Der Bot ist nun Online!')
+
+    bot.user.setPresence({
+        activity: {
+            name: 't!help',
+            type: 'PLAYING',
+        },
+        status: 'online'
+    })
+})
+
+bot.login(TOKEN)
